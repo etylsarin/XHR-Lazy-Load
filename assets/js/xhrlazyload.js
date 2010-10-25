7e138c589ba14b482c431a35278703cb4f03844c
maresf <maresf@uklon-06296.(none)>
2010-10-25 14:50:42 +0100

    FIXED css non-ajax load call the success callback instead of error callback even if the file returns 404 error FIXED css non-ajax load doesn't work in IE ADDED user could have possibility to set a timeout. That should fix Opera's error event bug (http://msdn.microsoft.com/en-us/library/cc304105%28v=VS.85%29.aspx) ADDED css media attribute is set to all. Should be dynamic, so user can change it

--- assets/js/xhrlazyload.js
+++ assets/js/xhrlazyload.js
@@ -21,10 +21,13 @@ Here is an example how to use xhr lazy loading, parameters and their values are 
 		type: 'js', //optional attribute, 'js' by default, other possible value is 'css'
 		fixOrder: true, //optional attribute, false by default, when set to true loading order of the related file is preserved
 		ajax: false, //optional attribute, true by default, decides if the source file is loaded by xhr
+		attributes: {id: 'myFile'}, //optional attribute, undefined by default, allows user to add specific attributes to assets
+		// allowed attributes for script tag: id
+		// allowed attributes for link or style tag: id, media, title
+		timeout: 5000, //optional attribute, 5 seconds by default, after the period fires error event so the rest of the queue can be processed
 		callback: myCallbackFunction //optional attribute, callback function evaluated if the loading proces is successful
 		error: myErrorFunction //optional attribute, error function evaluated if the loading proces is unsuccessful
 		})
-
 */
 
 var $XLL = (function () {
@@ -33,29 +36,30 @@ var $XLL = (function () {
 	var queue = [],
 		queueProcessIndex = 0,
 		// define the default options attributes
-		optionsProperties = ['src', 'type', 'fixOrder', 'ajax', 'callback', 'error'],
+		optionsProperties = ['src', 'type', 'fixOrder', 'ajax', 'attributes', 'timeout', 'callback', 'error'],
 		// define the default options values
 		optionsDefaults = {
 			type: 'js',
 			fixOrder: false,
 			ajax: true,
+			timeout: 5000,
 			loadError: -1,
 			processed: -1
 		},
-		// find out whether the XMLHttpRequest method is defined for actual browser. If not, try to find propriate one.
+		jsAttributes = ['id'],
+		cssAttributes = ['id', 'media', 'title'],
+		// find out whether the XMLHttpRequest method is defined for actual browser. If not, try to use browser specific one.
 		XMLHttpRequest = (function () {
-			if (typeof window.XMLHttpRequest !== 'undefined') {
+			if (typeof window.XMLHttpRequest === 'function') {
 				return function () {
 					return new window.XMLHttpRequest();
 				};
-			} else if (typeof window.ActiveXObject !== 'undefined') {
+			} else if (typeof window.ActiveXObject === 'function') {
 				return function () {
 					return new window.ActiveXObject('Microsoft.XMLHTTP');
 				};
 			} else {
-				return function () {
-					return false;
-				};
+				return undefined;
 			}
 		}()),
 
@@ -73,14 +77,11 @@ var $XLL = (function () {
 		 */
 		extendObject = function extendObject(obj, prototypeObj, propertiesArray) {
 			var TempObj = function () {},
-				returnObj = {},
-				objLength = 0,
-				i = 0;
-			if (typeof prototypeObj === 'object') {
-				TempObj.prototype = prototypeObj;
-				returnObj = new TempObj();
-			}
-			if ((typeof obj === 'object') && (typeof propertiesArray === 'object')) {
+				returnObj, objLength, i;
+
+			TempObj.prototype = prototypeObj;
+			returnObj = new TempObj();
+			if (typeof obj === 'object') {
 				objLength = propertiesArray.length;
 				for (i = objLength; i--;) {
 					if (obj[propertiesArray[i]] !== undefined) {
@@ -101,57 +102,64 @@ var $XLL = (function () {
 		 */
 		createNode = function createNode(options) {
 			var d = document,
-				node = null,
-				attr = null,
-				returnValue = false;
+				node,
+				addCustomAttributes = function addCustomAttributes(currentNode, attributes, filterArray) {
+					var len = filterArray.length,
+						i = 0,
+						currentAttribute = null;
+					if (typeof attributes === 'object') {
+						for (i = len; i--;) {
+							currentAttribute = attributes[filterArray[i]];
+							if (currentAttribute !== undefined) {
+								currentNode.setAttribute(filterArray[i], currentAttribute);
+							}
+						}
+					}
+				};
 
-			if ((typeof options === 'object') && (typeof options.type === 'string') && ((typeof options.src === 'string') || (typeof options.content === 'string'))) {
+			if ((typeof options.src === 'string') || (typeof options.content === 'string')) {
 				switch (options.type) {
 				case 'js':
 					// creates a script node
 					node = d.createElement('script');
 					node.setAttribute('type', 'text/javascript');
-					if ((options.ajax === false) && (typeof options.src === 'string')) {
-						node.setAttribute('src', options.src);
-						returnValue = node;
-					} else if (typeof options.content === 'string') {
+					addCustomAttributes(node, options.attributes, jsAttributes);
+					if (typeof options.content === 'string') {
 						//IE can't append node to the script element
-						if ((typeof node.canHaveChildren === 'undefined') || (node.canHaveChildren === true)) {
+						if (node.canHaveChildren !== false) {
 							node.appendChild(d.createTextNode(options.content));
 						} else {
 							node.text = options.content;
 						}
-						returnValue = node;
+					} else {
+						node.setAttribute('src', options.src);
 					}
 					break;
 				case 'css':
 					// creates a link (stylesheet) node
-					if ((options.ajax === false) && (typeof options.src === 'string')) {
-						node = d.createElement('link');
-						node.setAttribute('media', 'all');
-						node.setAttribute('type', 'text/css');
-						node.setAttribute('rel', 'stylesheet');
-						node.setAttribute('href', options.src);
-						returnValue = node;
-					} else if (typeof options.content === 'string') {
+					if (typeof options.content === 'string') {
 						node = d.createElement('style');
-						node.setAttribute('type', 'text/css');
 						//IE can't append node to the style element
-						if (typeof node.styleSheet === 'undefined') {
+						if (node.styleSheet === undefined) {
 							node.appendChild(d.createTextNode(options.content));
 						} else {
-							node.styleSheet.cssText = d.createTextNode(options.content);
+							node.styleSheet.cssText = options.content;
 						}
-						returnValue = node;
+					} else {
+						node = d.createElement('link');
+						node.setAttribute('rel', 'stylesheet');
+						node.setAttribute('href', options.src);
 					}
+					node.setAttribute('type', 'text/css');
+					addCustomAttributes(node, options.attributes, cssAttributes);
 					break;
 				}
 			}
-			return returnValue;
+			return node;
 		},
 
 		/**
-		 * Creates and returns a hidden iframe and injects passed content into it.
+		 * Creates and returns a hidden iframe.
 		 *
 		 * @method createIframe
 		 * @param {HTMLElement} headContent store an element to inject into the iframe head
@@ -159,30 +167,23 @@ var $XLL = (function () {
 		 * @return {Object}
 		 * @private
 		 */
-		createIframe = function createIframe(injectToHead, injectToBody) {
-			var iframe = document.createElement("iframe"),
-				d = null;
+		createIframe = function createIframe(parentElement) {
+			var iframe = document.createElement('iframe'),
+				d;
 
 			// set iframe as a non-displayed element to avoid repaints and reflows
 			iframe.style.display = 'none';
-			document.body.appendChild(iframe);
-			d = iframe.document;
-			if (iframe.contentDocument) { // modern browsers
+			parentElement.appendChild(iframe);
+			if (iframe.contentDocument !== undefined) { // modern browsers
 				d = iframe.contentDocument;
-			} else if (iframe.contentWindow) { // IE5.5 and IE6
-				d = iframe.contentWindow.document;
+			} else if (iframe.contentWindow !== undefined) { // IE5.5 and IE6
+				d = iframe.contentWindow;
 			}
-			// Inject the content in the iframe
-			d.open();
-			if (typeof injectToHead === 'object') {
-				d.getElementsByTagName('head')[0].appendChild(injectToHead);
+			if (d.document !== undefined) {
+				d = d.document;
 			}
-			if (typeof injectToBody === 'object') {
-				d.getElementsByTagName('body')[0].appendChild(injectToBody);
-			}
-			d.close();
 			// return iframe node and its document object
-			return {iframe: iframe, iframeDocument: d};
+			return {iframeNode: iframe, iframeDocument: d};
 		},
 
 		/**
@@ -195,40 +196,37 @@ var $XLL = (function () {
 		 * @private
 		 */
 		xhrload = function xhrload(options, callback) {
-			var xhr = null;
+			var xhr, asyncTimeout;
 
 			// test whether the XMLHttpRequest is defined and options variable is an object
-			if ((typeof XMLHttpRequest === 'function') && (typeof options === 'object')) {
+			if (typeof XMLHttpRequest === 'function') {
 				xhr = new XMLHttpRequest();
 				options.loadError = 2;
+				// timeout for requests over an unreliable network
+				asyncTimeout = window.setTimeout(function () {
+					xhr.abort();
+					callback(options);
+				}, options.timeout);
 				xhr.onreadystatechange = function () {
 					// readyState 4 menas complete
 					if (xhr.readyState === 4) {
+						window.clearTimeout(asyncTimeout);
 						if ((xhr.status >= 200) && (xhr.status < 300) || (xhr.status === 304)) {
 							options.content = xhr.responseText;
 							options.loadError = 0;
+						// something went wrong with XMLHttpRequest
 						} else if (xhr.status === 0) {
 							options.loadError = 1;
 						}
-						if (typeof callback === 'function') {
-							callback(options);
-						}
+						callback(options);
 					}
 				};
 				xhr.open('GET', options.src, true);
 				xhr.send(null);
 			} else {
-				// test if the options variable is an object
-				if (typeof options !== 'object') {
-					options = {};
-					options.loadError = 2;
-				// otherwise the XMLHttpRequest is not defined
-				} else {
-					options.loadError = 1;
-				}
-				if (typeof callback === 'function') {
-					callback(options);
-				}
+				// the XMLHttpRequest is not defined
+				options.loadError = 1;
+				callback(options);
 			}
 		},
 
@@ -243,6 +241,7 @@ var $XLL = (function () {
 		 */
 		injectToDOM = function injectToDOM(currentOptions, callback) {
 			var currentNode = createNode(currentOptions),
+				iframeNode, iframeDocument, syncTimeout,
 				isLoaded = false,
 				errorOccured = false,
 				d = document,
@@ -250,12 +249,12 @@ var $XLL = (function () {
 				bodyElement = d.getElementsByTagName('body')[0],
 				// function which is called after the node's injection
 				onLoadComplete = function onLoadComplete(currentNode, currentOptions, callback) {
-					if ((currentNode.iframe !== undefined) && (currentNode.iframeDocument !== undefined)) {
-						headElement.appendChild(currentNode.iframeDocument.getElementsByTagName('link')[0]);
-						bodyElement.removeChild(currentNode.iframe);
-					}
+					window.clearTimeout(syncTimeout);
 					isLoaded = true;
 					if (errorOccured === false) {
+						if (iframeDocument !== undefined) {
+							headElement.appendChild(iframeDocument.getElementsByTagName('link')[0]);
+						}
 						// when no error occured call the users callback
 						if (typeof currentOptions.callback === 'function') {
 							currentOptions.callback(currentOptions);
@@ -266,42 +265,79 @@ var $XLL = (function () {
 							currentOptions.error(currentOptions);
 						}
 					}
-					// call the callback parameter
-					if (typeof callback === 'function') {
-						callback(errorOccured);
+					if (iframeNode !== undefined) {
+						bodyElement.removeChild(iframeNode);
 					}
+					// call the function's callback
+					callback(errorOccured);
 				};
 
-			// if the asset has been downloaded by ajax simply inject the node into the page
-			if (currentOptions.ajax) {
-				headElement.appendChild(currentNode);
-				onLoadComplete(currentNode, currentOptions, callback);
-			// otherwise test the asset's type
-			} else {
-				if (currentOptions.type === 'js') {
-					// insert the script link
-					headElement.appendChild(currentNode);
-				} else if (currentOptions.type === 'css') {
-					// if the type is css it is neccessary to create an iframe because link tag has no onload event by its own
-					currentNode = createIframe(currentNode);
-				}
-				// and create onload event
-				currentNode.onload = function () {
-					if (!isLoaded) {
-						onLoadComplete(currentNode, currentOptions, callback);
-					}
-				};
-				// and onload event for IE
-				currentNode.onreadystatechange = function () {
-					if (!isLoaded && ((currentNode.readyState === 'loaded') || (currentNode.readyState === 'complete'))) {
-						onLoadComplete(currentNode, currentOptions, callback);
-					}
-				};
-				// in case of aving problems with loading the linked file
-				currentNode.onerror = function () {
+			if (currentNode !== undefined) {
+				// after the period fires error event so the rest of the queue can be processed in case it's being held by event firing issue
+				syncTimeout = window.setTimeout(function () {
 					errorOccured = true;
 					onLoadComplete(currentNode, currentOptions, callback);
-				};
+				}, currentOptions.timeout);
+				// if the asset has been downloaded by ajax simply inject the node into the head of a page
+				if (currentOptions.ajax) {
+					headElement.appendChild(currentNode);
+					onLoadComplete(currentNode, currentOptions, callback);
+				// otherwise test the asset's type
+				} else {
+					// IE doesn't support addEventListener up to version 9
+					if ((currentOptions.type === 'css') && (typeof window.addEventListener === 'function')) {
+						// if the type is css it is neccessary to create an iframe because link tag has no onload event by its own
+						iframeNode = createIframe(bodyElement);
+						iframeDocument = iframeNode.iframeDocument;
+						iframeNode = iframeNode.iframeNode;
+						// Inject the assets to the iframe
+						iframeDocument.open();
+						iframeDocument.getElementsByTagName('head')[0].appendChild(currentNode);
+						iframeDocument.close();
+						currentNode = iframeNode;
+					// in case of js file or IE browser simply add the assets to the head of a page
+					} else {
+						headElement.appendChild(currentNode);
+					}
+					// IE doesn't support addEventListener up to version 9
+					if (typeof window.addEventListener === 'function') {
+						// and create onload event
+						currentNode.addEventListener('load', function XLLload() {
+							var loadedStyleSheet = null;
+							if (!isLoaded) {
+								if ((currentOptions.type === 'css') && (iframeDocument !== undefined)) {
+									loadedStyleSheet = iframeDocument.styleSheets[0];
+									if ((loadedStyleSheet.cssRules === undefined) || (loadedStyleSheet.cssRules.length === 0)) {
+										errorOccured = true;
+									}
+								} else if (currentOptions.type === 'css') {
+									errorOccured = true;
+								}
+								onLoadComplete(currentNode, currentOptions, callback);
+							}
+						}, false);
+						// in case of having problems with loading the linked file
+						currentNode.addEventListener('error', function XLLerror() {
+							errorOccured = true;
+							onLoadComplete(currentNode, currentOptions, callback);
+						}, false);
+					}
+					// onload event for IE, unfortunately the readyState is equal to loaded even in case of error occurrence
+					if (typeof currentNode.attachEvent === 'object') {
+						currentNode.attachEvent('onreadystatechange', function () {
+							var loadedStyleSheet = null;
+							if (!isLoaded && ((currentNode.readyState === 'loaded') || (currentNode.readyState === 'complete'))) {
+								if (currentOptions.type === 'css') {
+									loadedStyleSheet = currentNode.styleSheet;
+									if ((loadedStyleSheet.rules === undefined) || (loadedStyleSheet.rules.length === 0)) {
+										errorOccured = true;
+									}
+								}
+								onLoadComplete(currentNode, currentOptions, callback);
+							}
+						});
+					}
+				}
 			}
 		},
 
@@ -315,7 +351,7 @@ var $XLL = (function () {
 		 */
 		dequeue = function dequeue(currentQueue, currentQueueProcessIndex) {
 			var currentQueueLength = currentQueue.length,
-				currentQueueItem = null,
+				currentQueueItem,
 				blocked = false,
 				i = currentQueueProcessIndex;
 
@@ -332,7 +368,7 @@ var $XLL = (function () {
 						currentQueueItem.processed = 0;
 						// call the function which injects the actual asset into the DOM
 						injectToDOM(currentQueueItem, function (errorOccured) {
-							// define a callback of the inject function
+							// define a callback of the node inject function
 							if (errorOccured === false) {
 								var currentQueueProcessIndex = queueProcessIndex;
 								currentQueueItem.processed = 1;
@@ -372,16 +408,20 @@ var $XLL = (function () {
 				// push current item in the queue
 				currentQueue.push(currentOptions);
 				// try to load the asset using ajax
-				if (currentOptions.ajax === true) {
+				if (currentOptions.ajax) {
 					xhrload(currentOptions, function (currentOptions) {
 						// ajax call returns loadError equal to 1 when the assets are situated on a different server
-						if (currentOptions.loadError === 1) {
-							// in that case try to load it again without using ajax
+						if ((currentOptions.loadError === 1) && (currentOptions.hasOwnProperty('ajax') === false)) {
+							// in case the user doesn't set the ajax attribute try to load it again without using ajax
 							currentOptions.loadError = 0;
 							currentOptions.ajax = false;
 						}
 						if (currentOptions.loadError === 0) {
 							dequeue(currentQueue, queueProcessIndex);
+						}
+						// when error occured call the users error callback
+						if ((currentOptions.loadError > 0) && (typeof currentOptions.error === 'function')) {
+							currentOptions.error(currentOptions);
 						}
 					});
 				// load the asset without using ajax
